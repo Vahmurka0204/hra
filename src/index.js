f0354e2281b5195c10d667f5f7e65f8ec0cae8c0
Alexandra <sasha-021248@yandex.ru>
2023-10-02 20:27:26 +0200

    add save and load json

--- src/index.js
+++ src/index.js
@@ -9,4 +9,37 @@ window.addEventListener("load", () => {
     $icon.setAttribute("rel", "shortcut icon")
     $icon.setAttribute("href", `icons/${icons[index]}`)
     document.head.appendChild($icon)
+
+    const $data = document.getElementById("data")
+    const $save = document.getElementById("save")
+    const $load = document.getElementById("load")
+
+    $load.addEventListener("click", async () => {
+        const [fileHandler] = await window.showOpenFilePicker({
+            id: "json-data",
+            multiple: false,
+            startIn: "documents",
+            types: [{ description: "JSON documents", accept: { "application/json": ".json" } }],
+            excludeAcceptAllOption: false
+        })
+
+        const file = await fileHandler.getFile()
+        const content = await file.text()
+        $data.value = content
+    })
+
+    $save.addEventListener("click", async () => {
+        const fileHandler = await window.showSaveFilePicker({
+            id: "json-data",
+            suggestedName: "quiz.json",
+            startIn: "documents",
+            types: [{ description: "JSON documents", accept: { "application/json": ".json" } }],
+            excludeAcceptAllOption: false
+        })
+
+        const content = $data.value
+        const file = await fileHandler.createWritable()
+        await file.write(content)
+        await file.close()
+    })
 });
