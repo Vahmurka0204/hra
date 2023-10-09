49bb0766277b35fa7103708fa4092ca78ce86b38
Alexandra <sasha-021248@yandex.ru>
2023-10-09 21:25:53 +0200

    create table

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
+class Quiz {
+    constructor(name, themes){
+        this.name = name;
+        this.themes = themes;
+    }
+}
+
+class Theme {
+    constructor (name, questions){
+        this.name = name;
+        this.questions = questions;
+    }
+}
+
+class Question {
+    constructor(text, cost, answers){
+        this.text = text;
+        this.cost = cost;
+        this.answers = answers;
+    }
+}
+
+class Answer {
+    constructor(text, isItRight){
+        this.text = text;
+        this.isItRight = isItRight;
+    }
+}
+
 const getRandomIndex = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min);
 }
@@ -10,9 +39,33 @@ window.addEventListener("load", () => {
     $icon.setAttribute("href", `icons/${icons[index]}`)
     document.head.appendChild($icon)
 
-    const $data = document.getElementById("data")
+    const $data = document.getElementsByTagName("tbody")[0]
     const $save = document.getElementById("save")
     const $load = document.getElementById("load")
+
+    const ans1 = new Answer ("a", true)
+    const ans2 = new Answer ("b", false)
+    const question = new Question("question?", 100, [ans1, ans2])
+    const theme = new Theme ("Name", [question])
+    const quiz = new Quiz("nova hra", [theme])
+
+    for (let i = 0; i < quiz.themes.length; i++) {
+        const element = quiz.themes[i];
+        const $tr = document.createElement("tr")
+        const $td = document.createElement("td")
+        $td.innerHTML = element.name
+        $tr.append($td)
+
+        for (let j = 0; j < element.questions.length; j++) {
+            const $td = document.createElement("td")   
+            $td.innerHTML=element.questions[j].cost   
+            $tr.append($td)
+        }
+
+        $data.append($tr)
+        
+    }
+
 
     $load.addEventListener("click", async () => {
         const [fileHandler] = await window.showOpenFilePicker({
