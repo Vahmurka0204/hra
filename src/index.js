34f859580ddeeb7de172c02dbe9fae14eda6f0d9
Alexandra <sasha-021248@yandex.ru>
2023-10-26 21:55:07 +0200

    add modal

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 class Quiz {
-    constructor(name, themes){
+    constructor(name, themes) {
         this.name = name;
         this.themes = themes;
     }
 }
 
 class Theme {
-    constructor (name, questions){
+    constructor(name, questions) {
         this.name = name;
         this.questions = questions;
     }
 }
 
 class Question {
-    constructor(text, cost, answers){
+    constructor(text, cost, answers) {
         this.text = text;
         this.cost = cost;
         this.answers = answers;
@@ -21,7 +21,7 @@ class Question {
 }
 
 class Answer {
-    constructor(text, isItRight){
+    constructor(text, isItRight) {
         this.text = text;
         this.isItRight = isItRight;
     }
@@ -29,43 +29,57 @@ class Answer {
 
 const getRandomIndex = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min);
-}
+};
 
 window.addEventListener("load", () => {
     const icons = ["chameleon.svg", "toucan.svg", "sloth.svg"];
-    const $icon = document.createElement("link")
-    const index = getRandomIndex(0, icons.length)
-    $icon.setAttribute("rel", "shortcut icon")
-    $icon.setAttribute("href", `icons/${icons[index]}`)
-    document.head.appendChild($icon)
+    const $icon = document.createElement("link");
+    const index = getRandomIndex(0, icons.length);
+    $icon.setAttribute("rel", "shortcut icon");
+    $icon.setAttribute("href", `icons/${icons[index]}`);
+    document.head.appendChild($icon);
 
-    const $data = document.getElementsByTagName("tbody")[0]
-    const $save = document.getElementById("save")
-    const $load = document.getElementById("load")
+    const $tableBody = document.getElementsByTagName("tbody")[0];
+    const $save = document.getElementById("save");
+    const $load = document.getElementById("load");
+    const $new = document.getElementById("new");
+    const $modal = document.getElementById("modal");
 
-    const ans1 = new Answer ("a", true)
-    const ans2 = new Answer ("b", false)
-    const question = new Question("question?", 100, [ans1, ans2])
-    const theme = new Theme ("Name", [question])
-    const quiz = new Quiz("nova hra", [theme])
+    function DrawTable(quiz) {
+        for (let i = 0; i < quiz.themes.length; i++) {
+            const element = quiz.themes[i];
+            const $tr = document.createElement("tr");
+            const $td = document.createElement("td");
+            $td.innerHTML = element.name;
+            $tr.append($td);
 
-    for (let i = 0; i < quiz.themes.length; i++) {
-        const element = quiz.themes[i];
-        const $tr = document.createElement("tr")
-        const $td = document.createElement("td")
-        $td.innerHTML = element.name
-        $tr.append($td)
+            for (let j = 0; j < element.questions.length; j++) {
+                const $td = document.createElement("td");
+                $td.className = "question";
+                $td.innerHTML = element.questions[j].cost;
+                $td.addEventListener("click", () => {
+                    document.getElementById("question").innerText = element.questions[j].text;
 
-        for (let j = 0; j < element.questions.length; j++) {
-            const $td = document.createElement("td")   
-            $td.innerHTML=element.questions[j].cost   
-            $tr.append($td)
+                    element.questions[j].answers.forEach((value, index) => {
+                        document.getElementById(`answer-${index}`).innerText = value.text;
+                    });
+
+                    $modal.classList.add("open");
+                    const exits = $modal.querySelectorAll(".modal-exit");
+                    exits.forEach(function (exit) {
+                        exit.addEventListener("click", function (event) {
+                            event.preventDefault();
+                            $modal.classList.remove("open");
+                        });
+                    });
+                });
+                $tr.append($td);
+            }
+
+            $tableBody.append($tr);
+
         }
-
-        $data.append($tr)
-        
     }
-
 
     $load.addEventListener("click", async () => {
         const [fileHandler] = await window.showOpenFilePicker({
@@ -74,25 +88,56 @@ window.addEventListener("load", () => {
             startIn: "documents",
             types: [{ description: "JSON documents", accept: { "application/json": ".json" } }],
             excludeAcceptAllOption: false
-        })
+        });
 
-        const file = await fileHandler.getFile()
-        const content = await file.text()
-        $data.value = content
-    })
+        const file = await fileHandler.getFile();
+        const content = await file.text();
+        $tableBody.value = content;
+    });
 
     $save.addEventListener("click", async () => {
         const fileHandler = await window.showSaveFilePicker({
             id: "json-data",
-            suggestedName: "quiz.json",
+            suggestedName: "kviz.json",
             startIn: "documents",
             types: [{ description: "JSON documents", accept: { "application/json": ".json" } }],
             excludeAcceptAllOption: false
-        })
+        });
 
-        const content = $data.value
-        const file = await fileHandler.createWritable()
-        await file.write(content)
-        await file.close()
-    })
+        const content = $tableBody.value;
+        const file = await fileHandler.createWritable();
+        await file.write(content);
+        await file.close();
+    });
+
+    $new.addEventListener("click", () => {
+        const quizName = prompt("Nazev kviza:");
+        const numberThemes = parseInt(prompt("Kolik bude témat?"));
+        const numberQuestions = parseInt(prompt("Kolik bude otázek v každém tématu?"));
+
+        let quiz = new Quiz(quizName, Array(numberThemes));
+
+        for (let i = 0; i < numberThemes; i++) {
+            let qcost = 100;
+            const name = "Téma" + (i + 1);
+
+            let theme = new Theme(name, Array(numberQuestions));
+
+            for (let j = 0; j < numberQuestions; j++) {
+                let question = new Question("Text otázky", qcost, Array(4));
+                qcost += 100;
+
+                for (let k = 0; k < 4; k++) {
+                    let answer = new Answer("Varianta " + (k + 1), false);
+                    question.answers[k] = answer;
+                }
+
+                theme.questions[j] = question;
+
+            }
+            quiz.themes[i] = theme;
+        }
+
+        DrawTable(quiz);
+    });
 });
